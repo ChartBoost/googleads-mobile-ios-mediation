ef29530ee2800f0443e6b08dd65959c2f8b4bffd
Daniel Barros <daniel.barros@chartboost.com>
2019-06-13 15:48:10 +0200

    Fixed initialization bug, allowed for initialization retry

--- adapters/Chartboost/ChartboostAdapter/GADMAdapterChartboostSingleton.m
+++ adapters/Chartboost/ChartboostAdapter/GADMAdapterChartboostSingleton.m
@@ -66,6 +66,7 @@
     _completionHandlers = [[NSMutableArray alloc] init];
     _bannersToDelegates = [NSMapTable weakToWeakObjectsMapTable];
     _loadingBanners = [[NSMutableArray alloc] init];
+    _initState = UNINITIALIZED;
   }
   return self;
 }
@@ -73,21 +74,23 @@
 - (void)startWithAppId:(NSString *)appId
           appSignature:(NSString *)appSignature
      completionHandler:(ChartbbostInitCompletionHandler)completionHandler {
-  if (_initState == INITIALIZED) {
-    completionHandler(nil);
-    return;
-  }
-
-  static dispatch_once_t once;
-  dispatch_once(&once, ^{
-    [Chartboost startWithAppId:appId appSignature:appSignature delegate:self];
-    [Chartboost setMediation:CBMediationAdMob
-          withLibraryVersion:[GADRequest sdkVersion]
-              adapterVersion:kGADMAdapterChartboostVersion];
-    [Chartboost setAutoCacheAds:YES];
-  });
-  _initState = INITIALIZING;
-  [_completionHandlers addObject:completionHandler];
+    @synchronized (self) {
+        if (_initState == INITIALIZED) {
+            completionHandler(nil);
+            return;
+        }
+        
+        [_completionHandlers addObject:completionHandler];
+        
+        if (_initState != INITIALIZING) {
+            _initState = INITIALIZING;
+            [Chartboost startWithAppId:appId appSignature:appSignature delegate:self];
+            [Chartboost setMediation:CBMediationAdMob
+                  withLibraryVersion:[GADRequest sdkVersion]
+                      adapterVersion:kGADMAdapterChartboostVersion];
+            [Chartboost setAutoCacheAds:YES];
+        }
+    }
 }
 
 - (void)addRewardedAdAdapterDelegate:
